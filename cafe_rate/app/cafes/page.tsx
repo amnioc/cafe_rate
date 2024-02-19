664c19bd90260d19ac96a1f68a2e213b71e1960d
Nico Dhillon <nicdhillon19@gmail.com>
2024-02-19 16:36:00 +0000

    familiarisation with nextJS

---
+++ cafe_rate/app/cafes/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Cafes() {
+    return (
+      <main>
+        <h1>This will be Cafes</h1>
+        <Link href="/">Home</Link>
+      </main>
+    );
+  }
